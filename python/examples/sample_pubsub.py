b8ab1a376ee6a657451dbf9cb72948a6abb2ad4e
ahmad <ahmad@babylonai.dev>
2023-01-28 23:21:36 +0100

    added subscriber

---
+++ python/examples/sample_pubsub.py
@@ -0,0 +1,10 @@
+from tqdm import tqdm
+
+from babylog.pubsub import Subscriber
+
+sub = Subscriber(address='127.0.0.1', port=5555, topic='DEVICE_NAME')
+
+for i in tqdm(range(10)):
+    if sub.logged_data is not None:
+        print(sub.logged_data.detection)
+
--- python/src/babylog/pubsub.py
+++ python/src/babylog/pubsub.py
@@ -1,7 +1,10 @@
+import threading
+import queue
+
 import zmq
 
 from babylog.logger import babylogger
-
+from babylog.deserialize import LoggedPrediction
 
 class Publisher:
     def __init__(self, address: str, port: int, topic: str, context_io_threads: int = 1):
@@ -30,3 +33,65 @@ class Publisher:
 
     def shutdown(self):
         self._publisher.close()
+
+
+class Subscriber:
+    def __init__(self, address: str, port: int, topic: str, max_data_history: int = 10, context_io_threads: int = 1):
+        self._topic = topic
+        self._max_data_history = max_data_history
+        self._shutdown = False
+
+        self._context = zmq.Context(context_io_threads)
+        self._subscriber = zmq.Socket(self._context, zmq.SUB)
+        self._subscriber.setsockopt(zmq.RCVTIMEO, 1000)
+
+        try:
+            self._subscriber.connect(f'tcp://{address}:{port}')
+            self._subscriber.setsockopt_string(zmq.SUBSCRIBE, topic)
+            babylogger.info(f'subscriber with topic ({topic}) successfully connected to port {port}')
+        except Exception as e:
+            babylogger.error(f'could not setup subscriber: {e}')
+            raise
+        self._mutex = threading.Lock()
+        self._data = b''
+        self._data_queue = queue.Queue(maxsize=1000)
+        threading.Thread(target=self.receive).start()
+
+    def shutdown(self):
+        babylogger.info(f'shutting down {self._topic} stream.')
+        self._shutdown = True
+
+    def receive(self):
+        while not self._shutdown:
+            try:
+                if self._subscriber.poll(1000, zmq.POLLIN):
+                    recv_msgs = self._subscriber.recv_multipart()
+                    assert(len(recv_msgs) == 2)
+                    self._mutex.acquire()
+                    self._data = recv_msgs[1]
+                    print(self._data)
+                    self._data_queue.put(self._data)
+                    self._mutex.release()
+                else:
+                    pass
+            except Exception as e:
+                babylogger.error(f'could not receive message: {e}')
+        babylogger.info(f'{self._topic} stream shut down')
+
+    @property
+    def data(self):
+        self._mutex.acquire()
+        data = self._data
+        self._mutex.release()
+
+        return data
+
+    @property
+    def logged_data(self, max_timeout=1):
+        try:
+            data = self._data_queue.get(timeout=max_timeout)
+        except Exception as e:
+            babylogger.info(f'could not get message: {e}')
+            return None
+        return LoggedPrediction.from_bytes(data)
+
